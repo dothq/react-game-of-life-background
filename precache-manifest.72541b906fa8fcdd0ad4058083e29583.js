867614a8ac12b447ef46c76f8d261c13ca3a48d8
Martyn <zogacc@gmail.com>
2020-08-26 19:26:25 +0300

    Updates

---
+++ precache-manifest.72541b906fa8fcdd0ad4058083e29583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0874acd1d4772c1e2c9b30eae2b94778",
+    "url": "./index.html"
+  },
+  {
+    "revision": "00344462329e954930ce",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "8a850318e40bdd0f492f",
+    "url": "./static/js/2.984adc8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.984adc8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00344462329e954930ce",
+    "url": "./static/js/main.828da8b6.chunk.js"
+  },
+  {
+    "revision": "fddd0d3d1f9f94a147bf",
+    "url": "./static/js/runtime-main.29b91a05.js"
+  }
+]);
